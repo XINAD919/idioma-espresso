78c359377076a5ab47b6991823685403044f22d4
Daniel <da_919@hotmail.com>
2023-09-22 18:12:06 -0500

    se eliminan import y componentes inutiles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,19 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import DarkMode from "./DarkMode";
 import { useState } from "react";
 import Hamburger from "./icons/Hamburger";
-
-import Lang from "./icons/Lang";
 import Image from "next/image";
 
 const Header = () => {
   const router = useRouter();
-  const changeTo = router.locale === "es" ? "en" : "es";
-
   return (
     <div className='md:w-11/12 md:mx-auto md:py-3 header'>
       <div className='md:flex hidden justify-between items-center'>
         <div className='flex flex-row gap-1 items-center'>
           <div className='flex rounded-full h-fit w-fit'>
-            <Link href='#home' className=' flex items-center gap-2'>
+            <Link href='/' className=' flex items-center gap-2'>
               <Image
-                src='/idioma-espresso.png'
+                src={"/idioma-espresso.png"}
                 alt='logo idioma espresso'
                 width={80}
                 height={40}
@@ -26,12 +21,11 @@ const Header = () => {
             </Link>
           </div>
         </div>
-        <div className='flex gap-4 capitalize'>
+        <div className='link__group flex gap-4 capitalize'>
           <Link href={"#about-us"}>about us</Link>
           <Link href={"#producs-and-services"}>products and Services</Link>
           <Link href={"#locations"}>locations</Link>
           <Link href={"#videos"}>videos</Link>
-          <DarkMode />
         </div>
       </div>
       <HeaderMovile />
@@ -40,31 +34,34 @@ const Header = () => {
 };
 const HeaderMovile = () => {
   const router = useRouter();
-  const changeTo = router.locale === "es" ? "en" : "es";
   const [toggleMenu, setToggleMenu] = useState(true);
   const toggler = () => {
     setToggleMenu(!toggleMenu);
   };
 
   return (
-    <div className='md:hidden sm:flex mx-2 w-11/12 fixed'>
+    <div className='md:hidden sm:flex mx-2 w-11/12 '>
       <div className='flex justify-between  items-center w-full min-w-fit relative'>
         <div className='flex rounded-full h-12 w-12'>
           <Link href='/' className='flex items-center gap-2'>
-            Idioma Espresso
+            <Image
+              src='/idioma-espresso.png'
+              alt='logo idioma espresso'
+              width={80}
+              height={40}
+            />
           </Link>
         </div>
         <div className='flex flex-row-reverse gap-4'>
           <Hamburger onClick={toggler} />
-          <DarkMode />
         </div>
       </div>
       <div className={`${toggleMenu ? "hidden" : ""} top-14 w-full absolute`}>
         <div className='flex flex-col items-center text-left'>
-          <Link className='w-fit' href={``}></Link>
-          <Link className='w-fit' href={""}></Link>
-          <Link className='w-fit' href={``}></Link>
-          <Link className='w-fit' href={``}></Link>
+          <Link className="w-fit" href={"#about-us"}>about us</Link>
+          <Link className="w-fit" href={"#producs-and-services"}>products and Services</Link>
+          <Link className="w-fit" href={"#locations"}>locations</Link>
+          <Link className="w-fit" href={"#videos"}>videos</Link>
         </div>
       </div>
     </div>
